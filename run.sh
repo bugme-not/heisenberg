523f4ee37576147282a87180ce75306ad4a0d3fa
bugme-not <d4em0n35@gmail.com>
2026-09-01 11:50:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-
 /usr/local/bin/xray run -c /etc/xray.json &
-sleep 3
-exec /usr/local/sbin/haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+sleep 5
+exec docker-entrypoint.sh haproxy -f /usr/local/etc/haproxy/haproxy.cfg
