940fe8eda2c2516c0cef6f9700f90e9ba01411f5
bugme-not <d4em0n35@gmail.com>
2026-09-02 18:37:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,26 +7,11 @@ PORT="${PORT:-8080}"
 echo "CONTAINER STARTING"
 echo "PORT=${PORT}"
 
-echo "Checking Xray configuration"
-
-/usr/local/bin/xray run -test -c /etc/xray.json
-
-echo "Starting Xray"
-
-/usr/local/bin/xray run -c /etc/xray.json &
-XRAY_PID=$!
-
-sleep 2
-
-if ! kill -0 "$XRAY_PID" 2>/dev/null
-then
-echo "Xray failed to start"
-exit 1
-fi
-
 echo "Creating HAProxy configuration"
 
-sed "s/PORT/${PORT}/g" /usr/local/etc/haproxy/haproxy.cfg > /tmp/haproxy.cfg
+sed "s/__PORT__/${PORT}/g" \
+    /usr/local/etc/haproxy/haproxy.cfg \
+    > /tmp/haproxy.cfg
 
 echo "Checking HAProxy configuration"
 
@@ -34,4 +19,22 @@ haproxy -c -f /tmp/haproxy.cfg
 
 echo "Starting HAProxy"
 
-exec haproxy -db -f /tmp/haproxy.cfg
+haproxy -db -f /tmp/haproxy.cfg &
+HAPROXY_PID=$!
+
+sleep 1
+
+if ! kill -0 "$HAPROXY_PID" 2>/dev/null
+then
+    echo "HAProxy failed to start"
+    exit 1
+fi
+
+echo "Starting Xray"
+
+/usr/local/bin/xray run -c /etc/xray.json &
+XRAY_PID=$!
+
+echo "Xray PID=${XRAY_PID}"
+
+wait "$HAPROXY_PID"
